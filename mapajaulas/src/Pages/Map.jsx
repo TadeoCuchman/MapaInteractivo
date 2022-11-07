15a5882fc796a1b3e825264856a7adf90876907e
TadeoCuchman <tadeo.cuchman@gmail.com>
2022-11-07 11:31:36 -0300

    map functionalities
    map can mark zones and points only on front end

--- mapajaulas/src/Pages/Map.jsx
+++ mapajaulas/src/Pages/Map.jsx
@@ -5,48 +5,106 @@ import { MapContainer, TileLayer, Popup, Marker, Circle } from "react-leaflet";
 
 import { useMapEvents } from "react-leaflet/hooks";
 
+import './Map.css';
 import "leaflet/dist/leaflet.css";
 
-// const PopUp = ({ where }) => {
-//   const style = {
-//     width: "100px",
-//     height: "150px",
-
-//     border: "2px solid green",
-//     zIndex: "1000",
-//   };
-//   return <div style={style}>esto es un popup</div>;
-// };
 
 export const Map = () => {
-  const styles = {};
   const [state, setState] = useState({
     currentLocation: { lat: -34.90648499618397, lng: -56.1904071648873 },
     zoom: "22",
   });
-  const [showPopup, setShowPopup] = useState([]);
 
+  const [zoneBool, setZoneBool] = useState(false)
+  const [zone, setZone] = useState([]);
+  const [createPolygon, setCreatePolygon] = useState(false);
+  const [color, setColor] = useState('red');
+  const [markerTextPopup, setMarkerTextPopup] = useState(false);
+  const [markerTextSumbit, setMarkerTextSumbit] = useState(false);
+  const [markerTextInput, setMarkerTextInput] = useState('');
+  const [newWorkSpace, setNewWorkSpace] = useState(false);
+  
   function MyComponent() {
     const map = useMapEvents({
-      click: () => {
-        setShowPopup([]);
-        console.log(map, showPopup);
+      
+      click: (event) => {
+        if (newWorkSpace) {
+          
+        }
+        if (
+          (event.containerPoint.x > 370 || event.containerPoint.x < 0) ||
+          (event.containerPoint.y > 60 || event.containerPoint.y < 0)
+          ) {
+          setZone([...zone, event.latlng]);
+          console.log(zone)
+          if (markerTextSumbit) {
+            console.log(zone);
+            const circle = L.circle(zone[0], { radius: 5 }).addTo(map);
+            if (markerTextInput) {
+              circle.bindTooltip(markerTextInput).openTooltip();
+            }
+            document.getElementById("mapContainer").click();
+            setMarkerTextSumbit(false)
+            setMarkerTextPopup(false)
+            setZone([]);
+          }
+          if (zoneBool) {
+            L.polyline(zone, { color: color }).addTo(map);
+            if (createPolygon) {
+              L.polygon(zone, { color: color }).addTo(map);
+              setZoneBool(false);
+              setCreatePolygon(false);
+            }
+          }
+
+        }
       },
+
       locationfound: (location) => {
         console.log("location found:", location);
       },
-      contextmenu: (location) => {
+
+      contextmenu: (event) => {
+        setZone([event.latlng]);
+        console.log(zone)
+        L.popup({ closeButton: false })
+          .setLatLng(event.latlng)
+          .setContent(
+            zoneBool
+              ? "Marcar puntos de la zona"
+              : 
+            `<button id='markZone'> Marcar Zona </button>
+            <button id='markPoint'> Marcar Punto </button></br></br>
+            <button id='newWorkSpace'>Definir nuevo espacio de trabajo </button>
+            `
+          )
+          .openOn(map);
+        console.log(document.getElementById("markZone"))
+        document.getElementById("markZone").addEventListener("click", () => {
+          setZoneBool(true)
+        })
+
+        document.getElementById("markPoint").addEventListener("click", () => {
+          setMarkerTextPopup(true);
+          document.getElementById("mapContainer").click();
+        });
         
-        setShowPopup([location.latlng.lat, location.latlng.lng]);
-      },
+
+        document.getElementById("newWorkSpace").addEventListener("click", () => {
+            setNewWorkSpace(true);
+            document.getElementById("mapContainer").click();
+          });
+        
+      }
+      
     });
-    console.log(map);
+
     return null;
   }
 
-  const onClick = (event) => {
-    console.log("daleeeee", event);
-  };
+
+
+
 
   return (
     <div id="map">
@@ -57,24 +115,94 @@ export const Map = () => {
         minZoom={2}
         style={{ position: "relative" }}
       >
+        <div id="controls">
+          {zoneBool ? (
+            <>
+              <button
+                onClick={() => {
+                  setZoneBool(false);
+                  setCreatePolygon(false);
+                  window.location.reload(false);
+                }}
+              >
+                Cancelar Marco
+              </button>
+              <button
+                onClick={() => {
+                  setCreatePolygon(true);
+                  setTimeout(
+                    () => document.getElementById("mapContainer").click(),
+                    500
+                  );
+                }}
+              >
+                Terminar Marco
+              </button>
+              <ColorControls setColor={setColor} color={color} />
+            </>
+          ) : (
+            ""
+          )}
+          {markerTextPopup ? (
+            <>
+              <span>Nota</span>
+              <input
+                type="text"
+                id="markerTextPopup"
+                onChange={(event) => setMarkerTextInput(event.target.value)}
+                ></input>
+              <button
+                onClick={() => {
+                  setMarkerTextPopup(false)
+                }}
+              >
+                Cancelar
+              </button>
+              <button
+                onClick={() => {
+                  setMarkerTextPopup(false);
+                  setMarkerTextSumbit(true);
+                  setTimeout(
+                    () => document.getElementById("mapContainer").click(),
+                    500
+                  );
+                }}
+              >
+                Crear
+              </button>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
         <MyComponent />
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {showPopup.length > 0 ? (
-          <Circle
-            center={showPopup}
-            redius={200}
-            eventHandlers={{
-              click: onClick,
-            }}
-          >
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Circle>
-        ) : (
-          ""
-        )}
       </MapContainer>
     </div>
   );
 };
+
+const ColorControls = ({ setColor, color }) => {
+  const colors = ['red', 'green', 'blue'];
+
+
+  return (
+    <div id="colorControl">
+      <ul>
+        {colors.map((col) => {
+          return (
+            <li
+              className= "selected" 
+              style={{ backgroundColor: `${col}` }}
+              key={col}
+              onClick={() => setColor(col)}
+            >
+              {col}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
--- mapajaulas/src/Pages/WorkSpaces.jsx
+++ mapajaulas/src/Pages/WorkSpaces.jsx
@@ -1,5 +1,30 @@
-import * as React from "react";
+import React from "react";
+import { useState} from "react";
+
+import './WorkSpaces.css'
 
 export const WorkSpaces = () => {
-  return <div>workspaces</div>;
+  const [spaces, setSpaces] = useState([{latlang: [51,51],name: 'workplace' }])
+
+  return (
+    <div id='spaces'>
+      <h1>Espacios de trabajo:</h1>
+      <ul id='spacesList'>
+        {spaces.map((space) => 
+          <WorkSpace key={space.name} latlang={space.latlang} name={space.name} />
+        )}
+      </ul>
+    </div>);
+    
 };
+
+const WorkSpace = ({name, latlang }) => {
+  return (
+    <div id='space'>
+      {name} - {latlang}
+      <div id='minimap'>
+
+      </div>
+    </div>
+  )
+}
